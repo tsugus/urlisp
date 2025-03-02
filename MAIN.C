c65fbfeadd74e43b593600456881cacacc2ad079
Tsugu <146515030+tsugus@users.noreply.github.com>
2025-03-02 15:06:57 +0900

    Ver. 0.1.0
    Fixed an issue where an unquoted lambda expression given to 'def' would clear the environment list.
    
    Changes to be committed:
    	modified:   EVAL.C
    	modified:   MAIN.C
    	modified:   init.txt

--- MAIN.C
+++ MAIN.C
@@ -121,7 +121,7 @@ void greeting()
   printf("\n");
   printf("\t  A Minimal Pure LISP Interpreter  \n\n");
   printf("\t            U r L I S P            \n\n");
-  printf("\t           Version 0.0.8           \n");
+  printf("\t           Version 0.1.0           \n");
   printf("\tThis software is released under the\n");
   printf("\t            MIT License.           \n\n");
   printf("\t                     (C) 2025 Tsugu\n\n");
